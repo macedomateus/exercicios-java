7c3ffe343482ddb13d41a0c830b4a0b81393ab12
Mateus Macedo <maateusmacedo@gmail.com>
2023-05-09 18:00:30 -0300

    atualização

---
+++ src/br/com/javaexercicios/generation/MenorNumero.java
@@ -0,0 +1,16 @@
+package generation;
+
+public class MenorNumero {
+    public static void main(String[] args) {
+        int[] numeros = {-1,3,4,2,6};
+
+        int menorNumero = 0;
+
+        for (int numero : numeros) {
+            if(numero < menorNumero) {
+                menorNumero = numero;
+            }
+        }
+        System.out.println(menorNumero);
+    }
+}
