fbf8749fe23174ea43a854e1716a617ac982f0d1
Mateus Macedo <maateusmacedo@gmail.com>
2023-05-15 15:43:25 -0300

    atualização

--- src/br/com/javaexercicios/dio/basico/Ex01.java
+++ src/br/com/javaexercicios/dio/basico/Ex01.java
@@ -1,5 +1,7 @@
 package br.com.javaexercicios.dio.basico;
 
+import java.util.Scanner;
+
 /* O custo de um carro novo ao consumidor é a soma do custo de fábrica com a
 porcentagem do distribuidor e dos impostos (aplicados ao custo de fábrica).
 O gerente de uma loja de carros gostaria de um programa para calcular o preço
@@ -9,7 +11,7 @@ programa para ler esses valores e imprimir o valor final do carro.
 
 ---> Entrada <---
 Você receberá três valores inteiros que representam o custo de fábrica,
-as procentagens do distribuidor e os impostos.
+as porcentagens do distribuidor e os impostos.
 
 ---> Saída <---
 Como saída, teremos o valor final de um carro novo.
@@ -25,6 +27,28 @@ Saída:
 * */
 public class Ex01 {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
 
+        int custoFabrica,
+                porcentagemDistribuidor,
+                impostos,
+                carroNovo,
+                distribuidor,
+                imposto;
+
+
+        System.out.println("Digite o valor do custo de fábrica: ");
+        custoFabrica = scan.nextInt();
+        System.out.println("Digite o valor da porcentagem do distribuidor: ");
+        porcentagemDistribuidor = scan.nextInt();
+        System.out.println("Digite o valor dos impostos: ");
+        impostos = scan.nextInt();
+
+        distribuidor = (custoFabrica*porcentagemDistribuidor)/100;
+        imposto = (custoFabrica*impostos)/100;
+
+        carroNovo = custoFabrica+distribuidor+imposto;
+
+        System.out.println("O valor total do carro é: " + carroNovo + " reias.");
     }
 }
--- src/br/com/javaexercicios/generation/MenorNumero.java
+++ src/br/com/javaexercicios/generation/MenorNumero.java
@@ -1,8 +1,8 @@
-package generation;
+package br.com.javaexercicios.generation;
 
 public class MenorNumero {
     public static void main(String[] args) {
-        int[] numeros = {-1,3,4,2,6};
+        int[] numeros = {-1,3,-4,2,6};
 
         int menorNumero = 0;
 
